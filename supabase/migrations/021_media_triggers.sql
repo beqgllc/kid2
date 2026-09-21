297f290e33783bfdaffc0aee9cd2f7d829e1d586
beqgllc <bluecore.equity@gmail.com>
2026-09-21 17:18:14 -0400

    Delete supabase/migrations/021_media_triggers.sql

--- supabase/migrations/021_media_triggers.sql
+++
@@ -1,8 +0,0 @@
-drop trigger if exists touch_lyric_videos
-on public.lyric_videos;
-
-create trigger touch_lyric_videos
-before update
-on public.lyric_videos
-for each row
-execute procedure public.touch_updated_at();
